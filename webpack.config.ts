d8e641e1fd55a48fd06d3bc83c0cfedf9dbd17f8
Matthew Weidner <17693586+mweidner037@users.noreply.github.com>
2021-09-26 17:04:04 -0400

    add webpack

---
+++ webpack.config.ts
@@ -0,0 +1,49 @@
+import * as path from "path";
+import * as webpack from "webpack";
+import CopyWebpackPlugin from "copy-webpack-plugin";
+
+const config: webpack.Configuration = {
+  mode: "development",
+  devtool: "inline-source-map",
+  optimization: {
+    usedExports: true,
+    innerGraph: true,
+    sideEffects: true,
+  },
+  entry: "./src/site/renderer.ts",
+  output: {
+    filename: "renderer.js",
+    path: path.resolve(__dirname, "dist/site"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        // Copy index.html.
+        {
+          from: "./src/site/index.html",
+          to: "./[base]",
+        },
+      ],
+    }),
+  ],
+};
+
+export default config;
